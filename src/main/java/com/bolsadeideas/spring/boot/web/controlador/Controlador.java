0f0b65a2cdcee57cc6a09a616ebf0349580ad9b2
AgustinArielGalarza <aguusg015@gmail.com>
2020-01-12 20:24:53 -0300

    If, for each con tymeleaf

--- src/main/java/com/bolsadeideas/spring/boot/web/controlador/Controlador.java
+++ src/main/java/com/bolsadeideas/spring/boot/web/controlador/Controlador.java
@@ -1,12 +1,14 @@
 package com.bolsadeideas.spring.boot.web.controlador;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bolsadeideas.spring.boot.web.model.Usuario;
-import com.bolsadeideas.spring.boot.web.model.*;
 
 @Controller
 @RequestMapping("/app")
@@ -23,8 +25,18 @@ public class Controlador {
 		Usuario usr = new Usuario();
 		usr.setNombre("Agustín");
 		usr.setApellido("Galarza");
+		usr.setEmail("agustin.galarza96@gmail.com");
 		model.addAttribute("usuario", usr);
 		model.addAttribute("titulo","[Perfil del usuario ".concat(usr.getNombre())+"]");
 		return "perfil";
 	}
-}	
+	@RequestMapping("/listar")
+	public String listar(Model model) {
+		List<Usuario> usuarios = new ArrayList<>();
+		usuarios.add(new Usuario("Agustin","Galarza","agustin.galarza96@gmail.com"));
+		usuarios.add(new Usuario("Lautaro","Galarza","lautaro.galarza00@gmail.com"));
+		usuarios.add(new Usuario("Ariel","Galarza","Ariel.galarza86@gmail.com"));
+		model.addAttribute("usuarios",usuarios);
+		return "listar";
+	}
+}
