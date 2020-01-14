31dd7f4c2d6167138c3c3273e81bd4eea0251024
AgustinArielGalarza <agustin.galarza96@gmail.com>
2020-01-14 11:53:49 -0300

    RequestAtribute para poblar, path de paginas, links

--- src/main/java/com/bolsadeideas/spring/boot/web/controlador/Controlador.java
+++ src/main/java/com/bolsadeideas/spring/boot/web/controlador/Controlador.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bolsadeideas.spring.boot.web.model.Usuario;
@@ -32,11 +33,18 @@ public class Controlador {
 	}
 	@RequestMapping("/listar")
 	public String listar(Model model) {
+		model.addAttribute("titulo","Lista de usuarios");
+		return "listar";
+	}
+	
+	@ModelAttribute("usuarios")
+	public List<Usuario> poblarUsuarios(){
 		List<Usuario> usuarios = new ArrayList<>();
 		usuarios.add(new Usuario("Agustin","Galarza","agustin.galarza96@gmail.com"));
 		usuarios.add(new Usuario("Lautaro","Galarza","lautaro.galarza00@gmail.com"));
 		usuarios.add(new Usuario("Ariel","Galarza","Ariel.galarza86@gmail.com"));
-		model.addAttribute("usuarios",usuarios);
-		return "listar";
+		
+		return usuarios;
+		
 	}
 }
