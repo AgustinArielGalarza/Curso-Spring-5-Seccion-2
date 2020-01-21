86a0e76096b6061947361e71a9ccf16d8db03259
AgustinArielGalarza <aguusg015@gmail.com>
2020-01-21 07:13:29 -0300

    Css, properties y cosas staticas

--- src/main/java/com/bolsadeideas/spring/boot/web/controlador/Controlador.java
+++ src/main/java/com/bolsadeideas/spring/boot/web/controlador/Controlador.java
@@ -3,6 +3,7 @@ package com.bolsadeideas.spring.boot.web.controlador;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +16,16 @@ import com.bolsadeideas.spring.boot.web.model.Usuario;
 @RequestMapping("/app")
 public class Controlador {
 	
+	@Value("${text.indexController.index.titulo}")
+	private String textIndex;
+	@Value("${text.indexController.index.titulo}")
+	private String textPerfil;
+	@Value("${text.indexController.index.titulo}")
+	private String textListar;
+	
 	@GetMapping({"/","/home","/index"})
 	public String index(Model modelo){
-		modelo.addAttribute("titulo","[Curso de Spring boot con Model]");
+		modelo.addAttribute("titulo",textIndex);
 		return "index";
 	}
 	
@@ -28,12 +36,12 @@ public class Controlador {
 		usr.setApellido("Galarza");
 		usr.setEmail("agustin.galarza96@gmail.com");
 		model.addAttribute("usuario", usr);
-		model.addAttribute("titulo","[Perfil del usuario ".concat(usr.getNombre())+"]");
+		model.addAttribute("titulo",textPerfil.concat(usr.getNombre())+"]");
 		return "perfil";
 	}
 	@RequestMapping("/listar")
 	public String listar(Model model) {
-		model.addAttribute("titulo","Lista de usuarios");
+		model.addAttribute("titulo",textListar);
 		return "listar";
 	}
 	
